d52a7692752f2a883f21fcfa98425d879336df70
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-25 15:19:13 -0500

    Numeric operators

---
+++ operators.py
@@ -0,0 +1,26 @@
+# Python has several built-in data types, which can be classed as:
+#   > numeric
+#   > iterator
+#   > sequence (which are also iterators)
+#   > mapping
+#   > file
+#   > class
+#   > exception
+
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+print()
+
+for i in range(1, 4):
+    print(i)
+
+for i in range(1, a // b):
+    print(i)
