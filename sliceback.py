f36c4e15016d337930b5d5fb90660b87f52061ac
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-26 09:33:54 -0500

    Slicing backwards

---
+++ sliceback.py
@@ -0,0 +1,11 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[25::-1]
+print(backwards)
+
+backwards2 = letters[::-1]
+print(backwards2)
+
+print(letters[-10:-13:-1])
+print(letters[4::-1])
+print(letters[-1:-9:-1])
