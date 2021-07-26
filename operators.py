790068f847a6236db877f01211f7c1af9c7f0735
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-25 22:46:29 -0500

    Operator precedence

--- operators.py
+++ operators.py
@@ -29,3 +29,15 @@ for i in range(1, a // b):
 bun_price = 2.40
 money = 15
 print(money // bun_price)
+
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print((((a + b) / 3) - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+print(d)
+f = d = 4
+print(d)
