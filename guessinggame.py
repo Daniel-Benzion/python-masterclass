75a488abb606bdeba22f7cb23e871c1ff59e7db8
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-26 12:16:33 -0500

    Simple condition

--- guessinggame.py
+++ guessinggame.py
@@ -20,3 +20,17 @@ if tree1 == tree2:
     print("The trees are the same")
 else:
     print("The trees are different")
+
+x = 5
+y = 7
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is smaller than y")
+else:
+    print("x equals y")
+
+print()
+
+print("x equals y") if x == y else print("x is greater than y") if x > y else print("x is smaller than y")
