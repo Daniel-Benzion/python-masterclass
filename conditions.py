bcef42f32cb58d0bb5c2f2f17df0bd3a55c9b58e
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-26 13:40:45 -0500

    Simplify chained expressions

--- conditions.py
+++ conditions.py
@@ -9,3 +9,13 @@ if age < 16 or age > 65:
     print("Enjoy your freedom")
 else:
     print("Have a good day at work")
+
+if 16 <= age <= 65:
+    print("Have a good day at work")
+else:
+    print("Enjoy your freedom")
+
+if not(16 <= age <= 65):
+    print("Enjoy your freedom")
+else:
+    print("Have a good day at work")
