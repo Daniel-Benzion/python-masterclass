87baa7f31d69b55b6a034c9c34cb43002144a89d
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-26 09:37:32 -0500

    Slicing idioms

--- sliceback.py
+++ sliceback.py
@@ -8,4 +8,8 @@ print(backwards2)
 
 print(letters[-10:-13:-1])
 print(letters[4::-1])
-print(letters[-1:-9:-1])
+print(letters[:-9:-1])
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
