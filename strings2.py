f6ca772be3ab9a91c588ce0e36889c0e5c0c747e
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-25 23:22:21 -0500

    Using a step in a slice

--- strings2.py
+++ strings2.py
@@ -46,3 +46,15 @@ print(parrot[-4:-2])
 print(parrot[-4:12])
 
 print(parrot[-14:-8])
+
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+number = "9,223;372:036 854,775;807"
+print(number[1::4])
+
+separators = number[1::4]
+print(separators)
+
+values = "".join(char if char not in separators else " " for char in number).split()
+print([int(val) for val in values])
