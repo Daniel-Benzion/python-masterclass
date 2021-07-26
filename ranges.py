1be5bd19b83415c262bed1d019981920bec0d821
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-26 15:07:44 -0500

    for loops and ranges

---
+++ ranges.py
@@ -0,0 +1,21 @@
+for i in range(1, 20):
+    print("i is now {}".format(i))
+
+for i in range(0, 10):
+    print(i)
+
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(10, 0, -2):
+    print(i)
+
+age = int(input("How old are you?"))
+
+if age in range(16, 66):
+    print("Have a good day at work")
+else:
+    print("Enjoy your freedom")
+
+for i in range(0, 101, 7):
+    print(i)
--- strings2.py
+++ strings2.py
@@ -30,31 +30,53 @@
 # print(parrot[6 - 14])
 # print(parrot[8 - 14])
 
-parrot = "Norwegian Blue"
+# parrot = "Norwegian Blue"
 
-print(parrot[0:6])
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
-print(parrot[0:])
-print(parrot[10:])
+# print(parrot[0:6])
+# print(parrot[3:5])
+# print(parrot[0:9])
+# print(parrot[:9])
+# print(parrot[0:])
+# print(parrot[10:])
 
-print(parrot[:6] + parrot[6:])
-print(parrot[:])
+# print(parrot[:6] + parrot[6:])
+# print(parrot[:])
 
-print(parrot[-4:-2])
-print(parrot[-4:12])
+# print(parrot[-4:-2])
+# print(parrot[-4:12])
 
-print(parrot[-14:-8])
+# print(parrot[-14:-8])
 
-print(parrot[0:6:2])
-print(parrot[0:6:3])
+# print(parrot[0:6:2])
+# print(parrot[0:6:3])
 
-number = "9,223;372:036 854,775;807"
-print(number[1::4])
 
-separators = number[1::4]
-print(separators)
+# print(number[1::4])
+
+# separators = number[1::4]
+# print(separators)
+
+# number = "9,223;372:036 854,775;807"
+
+number = input("Please enter a series of numbers, using any separators you like: ")
+
+separators = ""
+
+for char in number:
+    if not char.isnumeric():
+        separators += char
+
 
 values = "".join(char if char not in separators else " " for char in number).split()
-print([int(val) for val in values])
+# print([int(val) for val in values])
+print(sum([int(val) for val in values]))
+
+quote = """
+Alright, but apart from the Sanitation, the Medicine, Education, Wine,
+Public Order, Irrigation, Roads, the Fresh-Water System,
+and Public Health, what have the Romans ever done for us?
+"""
+
+for char in quote:
+    if char.isupper():
+        print(char)
