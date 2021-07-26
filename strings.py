ccbfe0aba93beab3a23f1de19b120eb6c0e4d155
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-26 10:43:14 -0500

    f-strings

--- strings.py
+++ strings.py
@@ -19,3 +19,8 @@ print(type(age))
 
 # print(name + " is " + age + " years old.")
 print(name + " is " + str(age) + " years old.")
+
+print(name + f"is {age} years old")
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
