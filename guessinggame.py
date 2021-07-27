03a76220a3b95425a49f4b052af487365e8662e1
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-26 19:06:03 -0500

    Hi/Lo

--- guessinggame.py
+++ guessinggame.py
@@ -1,4 +1,6 @@
-answer = 5
+import random
+
+answer = random.randint(1, 10)
 
 print("Guess a number between 1 and 10: ")
 
@@ -13,24 +15,24 @@ while True:
         print("You got it!")
         break
 
-tree1 = 'Oak'
-tree2 = 'Ash'
+# tree1 = 'Oak'
+# tree2 = 'Ash'
 
-if tree1 == tree2:
-    print("The trees are the same")
-else:
-    print("The trees are different")
+# if tree1 == tree2:
+#     print("The trees are the same")
+# else:
+#     print("The trees are different")
 
-x = 5
-y = 7
+# x = 5
+# y = 7
 
-if x > y:
-    print("x is greater than y")
-elif x < y:
-    print("x is smaller than y")
-else:
-    print("x equals y")
+# if x > y:
+#     print("x is greater than y")
+# elif x < y:
+#     print("x is smaller than y")
+# else:
+#     print("x equals y")
 
-print()
+# print()
 
-print("x equals y") if x == y else print("x is greater than y") if x > y else print("x is smaller than y")
+# print("x equals y") if x == y else print("x is greater than y") if x > y else print("x is smaller than y")
---
+++ hilo.py
@@ -0,0 +1,38 @@
+low = 1
+high = 1000
+
+print("Please think of a number between {} and {}".format(low, high))
+input("Press ENTER to start")
+
+guesses = 1
+while True:
+    print("\tGuessing in the range of {} to {}".format(low, high))
+    guess = low + (high - low) // 2
+    high_low = input("My guess is {}. Should I guess higher or lower? "
+                     "Enter h or l, or c if my guess was correct"
+                     .format(guess)).casefold()
+
+    if high_low == "h":
+        low = guess + 1
+    elif high_low == "l":
+        high = guess - 1
+    elif high_low == "c":
+        print("I got it in {} guesses!".format(guesses))
+        break
+    else:
+        print("Please enter h, l or c")
+
+    guesses += 1
+
+
+
+
+
+number = 5
+multiplier = 8
+answer = 0
+
+for i in range(multiplier):
+    answer += number
+ 
+print(answer)
