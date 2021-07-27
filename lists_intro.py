20502fcfe66117a2483c7df4c3975e9af2fd1971
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-27 11:36:32 -0500

    Intro to lists

---
+++ lists_intro.py
@@ -0,0 +1,10 @@
+computer_parts = ["computer", "monitor", "keyboard", "mouse", "mousepad"]
+
+for part in computer_parts:
+    print(part)
+
+print()
+
+print(computer_parts[2])
+print(computer_parts[0:3])
+print(computer_parts[-1])
