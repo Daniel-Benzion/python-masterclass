7aaf868ce46718962c594a327bd3d0ef2f2923af
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-27 14:45:01 -0500

    Sorting

--- number_lists.py
+++ number_lists.py
@@ -12,3 +12,33 @@ print(len(odd))
 
 print("Mississippi".count("s"))
 print("Mississippi".count("iss"))
+
+
+even.extend(odd)
+print(even)
+
+
+even.sort()
+print(even)
+
+even.sort(reverse=True)
+print(even)
+
+even2 = sorted(even)
+
+nums = [2.3, 21, 16, 1.7]
+
+print(sorted(nums))
+
+
+test = ["a", "n", "b", "p", "g", "d", "t"]
+
+test2 = sorted(test)
+
+print(test)
+print(test2)
+
+print()
+
+print(id(test))
+print(id(test2))
