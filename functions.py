c95e0b57ecda9bc5b7b2e4b2ba6d57240e0f2e8a
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-29 16:20:49 -0500

    Palindrome sentence challenge

--- functions.py
+++ functions.py
@@ -7,7 +7,13 @@ def is_palindrome(word):
     word = word.casefold()
     return word == word[::-1]
 
+
+def is_palindrome_sentence(sentence):
+    result = "".join(char.casefold() for char in sentence if char.casefold() in "abcdefghijklmnopqrstuvwxyz")
+    return result == result[::-1]
+
 answer = multiply(10.5, 4)
 print(answer)
 
 print(is_palindrome("Tacocat"))
+print(is_palindrome_sentence("A man a plan, a canal: panama."))
