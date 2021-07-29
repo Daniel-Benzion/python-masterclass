9f6dfe451ded4dddf1256d7cb0d2b200863f84f0
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-29 16:13:43 -0500

    Palindromes

--- functions.py
+++ functions.py
@@ -2,5 +2,12 @@ def multiply(x, y):
     result = x * y
     return result
 
+
+def is_palindrome(word):
+    word = word.casefold()
+    return word == word[::-1]
+
 answer = multiply(10.5, 4)
 print(answer)
+
+print(is_palindrome("Tacocat"))
