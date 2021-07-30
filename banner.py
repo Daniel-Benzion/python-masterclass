f36abc35e3f12cd03940603da23d74d5b33bd0ff
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-30 12:24:05 -0500

    Handling invalid args

--- banner.py
+++ banner.py
@@ -1,8 +1,6 @@
-def banner_text(text):
-    screen_width = 80
+def banner_text(text, screen_width):
     if len(text) > screen_width - 4:
-        print("EEK!!")
-        print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED WIDTH")
+        raise ValueError("String {} is larger than specified width {}".format(text, screen_width))
 
     if text == "*":
         print("*" * screen_width)
@@ -19,7 +17,7 @@ banner_text("There's something you've forgotten!")
 banner_text("And that's to laugh and smile and dance and sing,")
 banner_text(" ")
 banner_text("When you're feeling in the dumps,")
-banner_text("Don't be silly chumps,")
+banner_text("Don't be silly chumps,") 
 banner_text("Just purse your lips and whistle - that's the thing!")
 banner_text("And... always look on the bright side of life...")
 banner_text("*")
